25160b04e6abd9332b182cdccf74b656d213116f
AndreAlexander-SM <124051521@upq.edu.mx>
2026-02-13 20:32:38 -0600

    Segunda parte de la practica
    implementamos, get, put, delete

--- miAPI/app/main.py
+++ miAPI/app/main.py
@@ -1,11 +1,12 @@
 #Importaciones
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException
 import asyncio
 from typing import Optional
 
+
 # Instancia del servidor
 app = FastAPI(
-    title='Mi primer API',
+    title='Mi primer API 2',
     description='Andre Alexander Sierra Martinez',
     version='1.0.0'
     )
@@ -30,11 +31,11 @@ async def hola():
         "estatus":"200"
         }
 
-@app.get("/v1/usuario/{id}",tags=['Parametro Obligatorio'])
+@app.get("/v1/parametroOb/{id}",tags=['Parametro Obligatorio'])
 async def consultaUno(id:int):
     return {"Se encontro usuario": id}
 
-@app.get("/v1/usuarios/",tags=['Parametro Opcional'])
+@app.get("/v1/parametroOp/",tags=['Parametro Opcional'])
 async def consultaTodos(id:Optional[int] = None):
     if id is not None:
         for usuario in usuarios:
@@ -43,3 +44,30 @@ async def consultaTodos(id:Optional[int] = None):
         return{"mensaje":"usuario no encontrado", "usuario": id}
     else:
         return{"mensaje":"No se proporciono id" }
+    
+@app.get("/v1/usuarios/",tags=['CRUD HTTP'])
+async def leer_usuarios():
+    return{
+        "status":"200",
+        "total": len(usuarios),
+        "usuarios":usuarios
+    }
+
+@app.post("/v1/usuarios/",tags=['CRUD HTTP'],status_code=status.HTTP_201_CREATED)
+async def crear_usuario(usuario:dict):
+    for usr in usuarios: 
+        if usr["id"] == usuario.get("id"):
+            raise HTTPException(
+                status_code=400,
+                detail="El id ya existe"
+            )
+    usuarios.append(usuario)
+    return{
+        "mensaje":"Usuario Agregado",
+        "Usuario":usuario
+    }
+
+@app.put("/v1/usuarios/",tags=['CRUD HTTP'], status_code=status.HTTP_204_NO_CONTENT)
+async def actualizar_usuario(usuario:dict):
+    for usr in usuarios:
+        if usr["id"] == usuario.get("id"):
