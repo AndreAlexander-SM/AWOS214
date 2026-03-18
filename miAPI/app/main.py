47363316e00934e09f08051f11e834b2ed2f5156
AndreAlexander-SM <124051521@upq.edu.mx>
2026-03-17 20:10:25 -0600

    Practica 8 terminada

--- miAPI/app/main.py
+++ miAPI/app/main.py
@@ -1,11 +1,6 @@
 #Importaciones
-from fastapi import FastAPI, status, HTTPException, Depends
-import asyncio
-from typing import Optional
-from pydantic import BaseModel, Field
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
-import secrets
-
+from fastapi import FastAPI
+from app.routers import usuarios, varios
 
 # Instancia del servidor
 app = FastAPI(
@@ -14,108 +9,5 @@ app = FastAPI(
     version='1.0.0'
     )
 
-#TB ficticia
-usuarios = [
-    {"id":1, "nombre" : "Juan", "edad":21},
-    {"id":2, "nombre" : "Israel", "edad":21},
-    {"id":3, "nombre" : "Sofi", "edad":21},
-]
-
-#Modelo de validacion Pydantic
-class usuario_create(BaseModel):
-    id:int = Field(...,gt=0, description="Identificador de usuario")
-    nombre:str = Field(..., min_length=3,max_length=50,example="Juanita")
-    edad:int = Field(..., ge=1, le=123, description="Edad valida entre 1 y 123")
-
-#Seguridad HTTP Basic
-
-security = HTTPBasic()
-
-def verificar_Peticion(credenciales: HTTPBasicCredentials=Depends(security)):
-   userAuth = secrets.compare_digest(credenciales.username, "Andre Alexander")
-   passAuth = secrets.compare_digest(credenciales.password, "2427")
-   
-   if not(userAuth and passAuth):
-      raise HTTPException(
-         status_code= status.HTTP_401_UNAUTHORIZED,
-         detail= "Credenciales no autorizadas"
-      )
-
-   return credenciales.username
-
-#Endpoints
-@app.get("/",tags=['Inicio'])
-async def bienvenida():
-    return {"mensaje": "Bienvenido a mi API!"}
-
-@app.get("/HolaMundo",tags=['Bienvenida Asincrona'])
-async def hola():
-    await asyncio.sleep(3) #simulacion de una peticion
-    return {
-        "mensaje": "Hola Mundo FastAPI!",
-        "estatus":"200"
-        }
-
-@app.get("/v1/parametroOb/{id}",tags=['Parametro Obligatorio'])
-async def consultaUno(id:int):
-    return {"Se encontro usuario": id}
-
-@app.get("/v1/parametroOp/",tags=['Parametro Opcional'])
-async def consultaTodos(id:Optional[int] = None):
-    if id is not None:
-        for usuario in usuarios:
-            if usuario["id"] == id:
-                return{"mensaje":"usuario encontrado", "usuario": usuario}
-        return{"mensaje":"usuario no encontrado", "usuario": id}
-    else:
-        return{"mensaje":"No se proporciono id" }
-    
-@app.get("/v1/usuarios/",tags=['CRUD HTTP'])
-async def leer_usuarios():
-    return{
-        "status":"200",
-        "total": len(usuarios),
-        "usuarios":usuarios
-    }
-
-@app.post("/v1/usuarios/",tags=['CRUD HTTP'],status_code=status.HTTP_201_CREATED)
-async def crear_usuario(usuario:usuario_create):
-    for usr in usuarios: 
-        if usr["id"] == usuario.id:
-            raise HTTPException(
-                status_code=400,
-                detail="El id ya existe"
-            )
-    usuarios.append(usuario)
-    return{
-        "mensaje":"Usuario Agregado",
-        "Usuario":usuario
-    }
-
-@app.put("/v1/usuarios/{id}", tags=['CRUD HTTP'])
-async def actualizar_usuario(id: int, usuario_actualizado: dict):
-    for index, usr in enumerate(usuarios):
-        if usr["id"] == id:
-            usuarios[index].update(usuario_actualizado)
-            return {
-                "mensaje": "Usuario actualizado correctamente",
-                "usuario": usuarios[index]
-            }
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail=f"El usuario con ID {id} no existe."
-    )
-
-@app.delete("/v1/usuarios/{id}", tags=['CRUD HTTP'])
-async def eliminar_usuario(id: int, userAuth= Depends(verificar_Peticion)):
-    for usuario in usuarios:
-        if usuario["id"] == id:
-            usuarios.remove(usuario)
-            return {
-                "mensaje": f"Usuario eliminado exitosamente {userAuth}",
-                "usuarios_restantes": len(usuarios)
-            }
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND, 
-        detail=f"No se pudo eliminar: El poñoñoin con ID {id} no existe"
-    )
+app.include_router(usuarios.router)
+app.include_router(varios.router)
